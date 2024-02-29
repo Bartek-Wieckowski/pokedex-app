9ae10a39af30b423c5d9a9036dd68a75e23854ce
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-29 10:14:54 +0100

    create basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { lazy, Suspense } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
+import ErrorFallback from './components/shared/ErrorFallback';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    errorElement: (
+      <ErrorFallback resetErrorBoundary={() => window.location.replace('/')} />
+    ),
+    element: <AppLayout />,
+    children: [
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+      {
+        path: '/',
+        element: <Homepage />,
+      },
+      // {
+      //   path: '/pokemon:name',
+      //   element: (
+      //     <Suspense fallback={<Loader />}>
+      //       <Clientspage />
+      //     </Suspense>
+      //   ),
+      // },
+    ],
+  },
+]);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,7 +48,7 @@ const App = () => {
       {import.meta.env.MODE === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
-      <h1>test</h1>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 };
