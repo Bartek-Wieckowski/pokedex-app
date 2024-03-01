db5918dae3e64d90f6ebe93fc1506ce6ce6affcd
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-01 10:08:35 +0100

    fix bg class

--- src/components/shared/PokemonList.tsx
+++ src/components/shared/PokemonList.tsx
@@ -7,7 +7,7 @@ type PokemonListProps = {
 
 const PokemonList = ({ pokemons }: PokemonListProps) => {
   return (
-    <div className="bg-white rounded-md mx-4 mt-8 mb-4 h-[300px] overflow-y-scroll custom-scrollbar">
+    <div className="bg-background rounded-md mx-4 mt-8 mb-4 h-[300px] overflow-y-scroll custom-scrollbar">
       <ul className="flex-center flex-wrap gap-2 py-2">
         {pokemons?.map((pokemon, index) => (
           <li key={pokemon.id}>
