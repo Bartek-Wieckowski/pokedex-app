fdce0dbe489e5540d391e9c76420d4337829a2cc
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-03 23:01:11 +0100

    create validators folder/file

---
+++ src/validators/validators.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+export const formSchema = z.object({
+    userInputValue: z.string().min(3, {
+      message: "Text must be at least 3 characters or try to click load more",
+    }),
+  });
