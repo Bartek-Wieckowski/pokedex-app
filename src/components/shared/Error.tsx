e14218b358337b39fca0efd654bea8693f64dc3a
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-03 22:56:00 +0100

    update code

--- src/components/shared/Error.tsx
+++ src/components/shared/Error.tsx
@@ -1,16 +1,21 @@
 import { Button } from '../ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 type ErrorProps = {
   msg: string;
 };
 
 const Error = ({ msg }: ErrorProps) => {
+  const location = useLocation();
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
     <>
       <h2 className="text-xl text-rose-500 mb-3">{msg}</h2>
-      <Button asChild variant="destructive">
-        <Link to="/">Try again</Link>
+      <Button asChild variant="destructive" onClick={refreshPage}>
+        <Link to={location.pathname}>Try again</Link>
       </Button>
     </>
   );
