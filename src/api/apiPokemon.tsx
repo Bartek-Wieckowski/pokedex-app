6cf7c46adfb7e8a71f5079c4ee3f556f95e9d609
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-03 22:53:32 +0100

    add function and small changes in exist functions

--- src/api/apiPokemon.tsx
+++ src/api/apiPokemon.tsx
@@ -1,15 +1,10 @@
 import axios from 'axios';
 import { BASE_API_URL } from './const';
-import { PokemonsType, singlePokemonType } from '@/types/types';
+import { PokemonByCategoryType, PokemonsType, SinglePokemonType } from '@/types/types';
 
-export async function getAllPokemons(
-  offset: number,
-  limit: number
-): Promise<PokemonsType[]> {
+export async function getAllPokemons(offset: number, limit: number): Promise<PokemonsType[]> {
   try {
-    const response = await axios.get(
-      `${BASE_API_URL}/pokemon/?offset=${offset}&limit=${limit}`
-    );
+    const response = await axios.get(`${BASE_API_URL}/pokemon/?offset=${offset}&limit=${limit}`);
     const pokemons = response.data.results;
 
     const detailedPokemons = await Promise.all(
@@ -26,14 +21,43 @@ export async function getAllPokemons(
   }
 }
 
-export async function getSinglePokemon(
-  pokemonId: number
-): Promise<singlePokemonType> {
+export async function getSinglePokemon(pokemonId: number): Promise<SinglePokemonType> {
   try {
     const response = await axios.get(`${BASE_API_URL}/pokemon/${pokemonId}`);
-    const singlePokemon = response.data
-    
+    const singlePokemon = response.data;
+
     return singlePokemon;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getPokemonByName(value: string): Promise<PokemonsType[]> {
+  try {
+    const response = await axios.get(`${BASE_API_URL}/pokemon/${value}`);
+    const pokemonByName = response.data;
+
+    return [pokemonByName];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getPokemonByType(value: string): Promise<PokemonsType[]> {
+  try {
+    const response = await axios.get(`${BASE_API_URL}/type/${value}`);
+    const pokemons = response.data.pokemon;
+
+    const detailedPokemons = await Promise.all(
+      pokemons.map(async (pokemon: PokemonByCategoryType) => {
+        const detailedResponse = await axios.get(pokemon.pokemon.url);
+        return detailedResponse.data;
+      })
+    );
+
+    return detailedPokemons;
   } catch (error) {
     console.error(error);
     throw error;
