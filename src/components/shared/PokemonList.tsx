a7b66f0097ed23118f9ad39eec0a2479ea8128f4
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-03 22:56:59 +0100

    changes display loader

--- src/components/shared/PokemonList.tsx
+++ src/components/shared/PokemonList.tsx
@@ -1,20 +1,29 @@
+import { useAppSelector } from '@/redux/hooks/hooks';
 import { PokemonsType } from '@/types/types';
 import PokemonItem from './PokemonItem';
+import Loader from './Loader';
 
 type PokemonListProps = {
   pokemons: PokemonsType[];
+  isFetching: boolean;
 };
 
-const PokemonList = ({ pokemons }: PokemonListProps) => {
+const PokemonList = ({ pokemons, isFetching }: PokemonListProps) => {
+  const userSearchValue = useAppSelector((state) => state.pokemons.userSearchValue);
+
   return (
-    <div className="bg-background mx-4 mt-8 mb-4 h-[300px] overflow-y-scroll custom-scrollbar">
-      <ul className="flex-center flex-wrap gap-2 py-2">
-        {pokemons?.map((pokemon) => (
-          <li key={pokemon.id}>
-            <PokemonItem pokemon={pokemon}/>
-          </li>
-        ))}
-      </ul>
+    <div className="bg-background mx-4 mt-8 mb-4 h-[300px] overflow-y-scroll custom-scrollbar relative">
+      {userSearchValue.length > 0 && isFetching ? (
+        <Loader />
+      ) : (
+        <ul className="flex-center flex-wrap gap-2 py-2">
+          {pokemons?.map((pokemon) => (
+            <li key={pokemon.id}>
+              <PokemonItem pokemon={pokemon} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
