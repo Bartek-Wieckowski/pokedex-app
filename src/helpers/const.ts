a307602c4ad24508bf6488841f1427f7920291c9
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-04 10:47:32 +0100

    add const settings of framer motion animation

--- src/helpers/const.ts
+++ src/helpers/const.ts
@@ -1,3 +1,30 @@
 export const USER_FILTER_BY_NAME = 'byName';
 export const USER_FILTER_BY_TYPE = 'byType';
 export const OFFSET_GET_POKEMONS = 0;
+
+export const MOTION_VARIANTS = {
+  initial: ({ direction }: { direction: 'forward' | 'backward' }) => ({
+    x: direction === 'backward' ? '-5%' : '5%',
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+    },
+  }),
+  in: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+    },
+  },
+  out: ({ direction }: { direction: 'forward' | 'backward' }) => ({
+    x: direction === 'backward' ? '5%' : '-5%',
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+    },
+  }),
+};
