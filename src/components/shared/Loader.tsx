81ef55ed5f4522ae93da28ea1592db101bc4f8cb
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-06 12:58:17 +0100

    add fallback props

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,11 +2,12 @@ import { BiLoaderAlt } from 'react-icons/bi';
 
 type LoaderProps = {
   type?: string;
+  fallbackLoader?: boolean;
 };
 
-const Loader = ({ type = 'text-5xl' }: LoaderProps) => {
+const Loader = ({ type = 'text-5xl', fallbackLoader = false }: LoaderProps) => {
   return (
-    <div className="flex-center w-full h-full">
+    <div className={`flex-center w-full h-full ${fallbackLoader && 'absolute'}`}>
       <BiLoaderAlt className={`animate-spin ${type}`} />
     </div>
   );
